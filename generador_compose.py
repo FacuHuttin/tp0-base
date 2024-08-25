5e37473f64dd5f55c2eba0a68c4ad9a85fef2860
FacuHuttin <fhuttin@fi.uba.ar>
2024-08-25 16:36:22 -0300

    Agrego generador the docker-compose

---
+++ generador_compose.py
@@ -0,0 +1,61 @@
+import sys
+
+def generate_base_content():
+    return """name: tp0
+services:
+  server:
+    container_name: server
+    image: server:latest
+    entrypoint: python3 /main.py
+    environment:
+      - PYTHONUNBUFFERED=1
+      - LOGGING_LEVEL=DEBUG
+    networks:
+      - testing_net
+      
+"""
+
+def generate_client_content(client_id):
+    return f"""  client{client_id}:
+    container_name: client{client_id}
+    image: client:latest
+    entrypoint: /client
+    environment:
+      - CLI_ID={client_id}
+      - CLI_LOG_LEVEL=DEBUG
+    networks:
+      - testing_net
+    depends_on:
+      - server
+
+"""
+
+def generate_network_content():
+    return """networks:
+  testing_net:
+    ipam:
+      driver: default
+      config:
+        - subnet: 172.25.125.0/24
+"""
+
+def generate_docker_compose(output_file, num_clients):
+  base_content = generate_base_content()
+  network_content = generate_network_content()
+
+  clients_content = "".join(generate_client_content(i) for i in range(1, num_clients + 1))
+
+  full_content = base_content + clients_content + network_content
+
+  with open(output_file, 'w') as f:
+    f.write(full_content)
+
+if __name__ == "__main__":
+  if len(sys.argv) != 3:
+    print("Usage: python3 generador_compose.py <output_file> <number_of_clients>")
+    sys.exit(1)
+
+  output_file = sys.argv[1]
+  num_clients = int(sys.argv[2])
+
+  generate_docker_compose(output_file, num_clients)
---
+++ generar-compose.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ "$#" -ne 2 ]; then
+  echo "Usage: $0 <output_file> <number_of_clients>"
+  exit 1
+fi
+
+OUTPUT_FILE=$1
+NUM_CLIENTS=$2
+
+echo "Nombre del archivo de salida: $OUTPUT_FILE"
+echo "Cantidad de clientes: $NUM_CLIENTS"
+
+python3 generador_compose.py $OUTPUT_FILE $NUM_CLIENTS
