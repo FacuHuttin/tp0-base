bb6563432cdb018b5e5c326269f267f6b9f7fa09
FacuHuttin <fhuttin@fi.uba.ar>
2024-08-31 16:23:54 -0300

    Agrego script para validar-echo-server

---
+++ validar-echo-server.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+SERVER_CONTAINER_NAME="server"
+SERVER_PORT="12345"
+NETWORK_NAME="tp0_testing_net"
+TEST_MESSAGE="Hello, Server!"
+RESULT="fail"
+
+RESPONSE=$(docker run --rm --network $NETWORK_NAME alpine sh -c "echo $TEST_MESSAGE | nc $SERVER_CONTAINER_NAME $SERVER_PORT")
+
+if [ "$RESPONSE" == "$TEST_MESSAGE" ]; then
+  RESULT="success"
+fi
+
+echo "action: test_echo_server | result: $RESULT"
