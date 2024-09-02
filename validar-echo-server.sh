f44161f131090275d171c51bb1d87cd027df7eab
FacuHuttin <fhuttin@fi.uba.ar>
2024-09-02 18:09:23 -0300

    arreglo de validar-echo-server

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -8,9 +8,7 @@ RESULT="fail"
 
 RESPONSE=$(docker run --rm --network $NETWORK_NAME alpine sh -c "echo $TEST_MESSAGE | nc $SERVER_CONTAINER_NAME $SERVER_PORT")
 
-echo "RESPONSE: $RESPONSE"
-
-if [ "$RESPONSE" == "$TEST_MESSAGE" ]; then
+if [ "$RESPONSE" = "$TEST_MESSAGE" ]; then
   RESULT="success"
 fi
 
