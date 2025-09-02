387b42d45de70cef3d83357e1f0c9da2182db15b
FacuHuttin <fhuttin@fi.uba.ar>
2025-09-01 22:54:40 -0300

    agrego variables de entorno en compose

--- generador-compose.py
+++ generador-compose.py
@@ -26,6 +26,8 @@ def generate_client_content(client_id):
     environment:
       - CLI_ID={client_id}
       - CLI_BETFILE=/agency.csv
+      - CLI_MAXRETRIES=5
+      - CLI_BASEBACKOFF=100ms
     volumes:
       - ./client/config.yaml:/config.yaml
       - ./.data/agency-{client_id}.csv:/agency.csv
